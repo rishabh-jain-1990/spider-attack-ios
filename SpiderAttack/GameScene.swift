d4ef6a0ff04b0824ce8288d8a188bafeb57848cd
rishabhjain <rishabh@bowstringstudio.in>
2016-09-03 18:19:30 +0530

    Broad level collision detection

--- SpiderAttack/GameScene.swift
+++ SpiderAttack/GameScene.swift
@@ -8,7 +8,7 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
 
     let background = SKSpriteNode(imageNamed: "BG")
     let blackWeb = SKSpriteNode(imageNamed: "BlackWeb")
@@ -19,7 +19,16 @@ class GameScene: SKScene {
     var spiderWidth : CGFloat = 1
     var countdownValueList = [Int]()
     
+    struct PhysicsCategory {
+        static let None      : UInt32 = 0
+        static let All       : UInt32 = UInt32.max
+        static let Spider   : UInt32 = 0b1       // 1
+        static let Bee: UInt32 = 0b10      // 2
+    }
+    
     override func didMoveToView(view: SKView) {
+        physicsWorld.gravity = CGVectorMake(0, 0)
+        physicsWorld.contactDelegate = self
         
         spiderWidth = size.width / NUM_SPIDERS
         
@@ -33,6 +42,13 @@ class GameScene: SKScene {
         var width = spiderWidth / BEE_SIZE_DIVIDER
         player.size = CGSize(width: width, height: width / aspectRatio)
         player.position = CGPoint(x: size.width / 2, y: player.size.height / 2 + 4)
+        player.physicsBody = SKPhysicsBody(rectangleOfSize: player.size)
+        player.physicsBody?.dynamic = true
+        player.physicsBody?.categoryBitMask = PhysicsCategory.Bee
+        player.physicsBody?.contactTestBitMask = PhysicsCategory.Spider
+        player.physicsBody?.collisionBitMask = PhysicsCategory.None
+        player.physicsBody?.usesPreciseCollisionDetection = true
+        
         addChild(player)
         
         spiderArray.removeAll()
@@ -51,6 +67,12 @@ class GameScene: SKScene {
             spiderArray[i].position = CGPoint(x: x, y: size.height)
             aspectRatio = spiderArray[i].size.width/spiderArray[i].size.height
             spiderArray[i].size = CGSize(width: spiderWidth, height: spiderWidth / aspectRatio)
+            spiderArray[i].physicsBody = SKPhysicsBody(rectangleOfSize: spiderArray[i].size) // 1
+            spiderArray[i].physicsBody?.dynamic = true // 2
+            spiderArray[i].physicsBody?.categoryBitMask = PhysicsCategory.Spider // 3
+            spiderArray[i].physicsBody?.contactTestBitMask = PhysicsCategory.Bee // 4
+            spiderArray[i].physicsBody?.collisionBitMask = PhysicsCategory.None // 5
+            
             addChild(spiderArray[i])
             addChild(line)
             
@@ -76,6 +98,33 @@ class GameScene: SKScene {
         myLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))
         
         self.addChild(myLabel)
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        
+        // 1
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        // 2
+        if ((firstBody.categoryBitMask & PhysicsCategory.Spider != 0) &&
+            (secondBody.categoryBitMask & PhysicsCategory.Bee != 0)) {
+            projectileDidCollideWithMonster(firstBody.node as! SKSpriteNode, monster: secondBody.node as! SKSpriteNode)
+        }
+        
+    }
+    
+    func projectileDidCollideWithMonster(projectile:SKSpriteNode, monster:SKSpriteNode) {
+        print("Hit")
+//        projectile.removeFromParent()
+//        monster.removeFromParent()
     }
     
     func getCountdownValue() -> Int
